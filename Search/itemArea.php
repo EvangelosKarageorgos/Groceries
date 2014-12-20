88acab0da72dbad73c50037ce6389f9d9aa80cbf
arximidisP <arximidis.p@gmail.com>
2014-12-20 20:28:06 +0200

    no message

--- Search/itemArea.php
+++ Search/itemArea.php
@@ -1,3 +1,24 @@
+
+
 <div class="itemArea">
-	<span>Item Area</span>
+
+	<?php
+		$products = array();
+		Application::getDB()->WhileReader("select * from products p inner join product_groups pg on pg.group_code = p.prod_group order by name", function(&$row) use(&$products){
+			$p = new Product();
+			$p->loadFromRow($row);
+			array_push($products, $p->toModel());
+		});
+		$list = array("items" => $products, "itemTemplate" => dirname(__FILE__)."/../Templates/ProductListItem.php");	
+		
+		echo renderTemplate(dirname(__FILE__)."/../Templates/List.php", $list);
+	?>
+	
 </div>
+
+
+
+
+
+
+	
