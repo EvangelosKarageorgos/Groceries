e3d722f39aea9a82aa07f8505a8740abada03bb1
arximidisP <arximidis.p@gmail.com>
2014-12-21 02:05:31 +0200

    no message

--- Templates/ProductListItem.php
+++ Templates/ProductListItem.php
@@ -1,11 +1,11 @@
 <div class="item">
 	<a href="<?= $model['url'] ?>">
-		<img src="<?= $model['imageUrl'] ?>" width="200" height="200" alt="<?= $model['name'] ?>" />
+		<img src="<?= $model['imageUrl'] ?>" width="100" height="100" alt="<?= $model['name'] ?>" />
 	</a>
 	<a href="<?= $model['url'] ?>">
 		<div class="name"><strong><?= $model['name'] ?></strong></div>
 	</a>
 	<div class="group"><?= $model['groupName'] ?></div>
 	<div class="description"><?= $model['description'] ?></div>
-	<div class="price"><?= $model['price'] ?></div>
+	<div class="price">Price: <?= $model['price'] ?>$</div>
 </div>
